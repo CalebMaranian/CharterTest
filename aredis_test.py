91409d35949ce4c95c4991588b7d1a14dccb4700
CalebMaranian <cmar1299@rams.colostate.edu>
2022-02-04 12:43:23 -0700

    Updated fast api and redis combo application with pylint and black code guidelines

--- aredis_test.py
+++ aredis_test.py
@@ -3,12 +3,14 @@ from aredis import StrictRedis
 from pydantic import BaseModel
 from pydantic import ValidationError
 
+
 class CPE4(BaseModel):
     ipv4_address: str
     port_end: int
     port_start: int
     speedboost_trace_id: str
     action: str
+
 
 class CPE6(BaseModel):
     ipv6_address: str
@@ -17,6 +19,7 @@ class CPE6(BaseModel):
     speedboost_trace_id: str
     action: str
 
+
 class Item(BaseModel):
     cm_mac: str
     qos_type: str
@@ -24,13 +27,14 @@ class Item(BaseModel):
     cpe_ipv4: CPE4
     cpe_ipv6: CPE6
 
+
 def fill_basemodel():
     cpe4_data = {
         "ipv4_address": "198.51.100.42",
         "port_end": 0,
         "port_start": 0,
         "speedboost_trace_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-        "action": "activate"
+        "action": "activate",
     }
     cpe4 = CPE4(**cpe4_data)
     cpe6_data = {
@@ -38,17 +42,18 @@ def fill_basemodel():
         "port_end": 0,
         "port_start": 0,
         "speedboost_trace_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-        "action": "activate"
+        "action": "activate",
     }
     cpe6 = CPE6(**cpe6_data)
     external_data = {
-          "cm_mac": "string",
-          "qos_type": "speedboost",
-          "router_ipv6_pd": "string",
-          "cpe_ipv4": cpe4,
-          "cpe_ipv6": cpe6
+        "cm_mac": "string",
+        "qos_type": "speedboost",
+        "router_ipv6_pd": "string",
+        "cpe_ipv4": cpe4,
+        "cpe_ipv6": cpe6,
     }
     return Item(**external_data)
+
 
 async def example():
     try:
@@ -56,20 +61,21 @@ async def example():
     except ValidationError as e:
         print(f"Failure {e.json()}")
 
-    client = StrictRedis(host='127.0.0.1', port=6379, db=0)
+    client = StrictRedis(host="127.0.0.1", port=6379, db=0)
 
     await client.flushdb()
-    await client.set('foo', model)
-    assert await client.exists('foo') is True
-    #await client.delete('foo')
-    #assert not await client.exists('foo')
+    await client.set("foo", model)
+    assert await client.exists("foo") is True
+    # await client.delete('foo')
+    # assert not await client.exists('foo')
 
-    #assert int(await client.get('foo')) == 101
-    #await client.expire('foo', 1)
-    #await asyncio.sleep(0.1)
-    #await client.ttl('foo')
-    #await asyncio.sleep(1)
-    #assert not await client.exists('foo')
+    # assert int(await client.get('foo')) == 101
+    # await client.expire('foo', 1)
+    # await asyncio.sleep(0.1)
+    # await client.ttl('foo')
+    # await asyncio.sleep(1)
+    # assert not await client.exists('foo')
+
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
--- fast_redis_combo.py
+++ fast_redis_combo.py
@@ -1,87 +1,140 @@
-from typing import Optional
+"""
+This module combines fastapi with redis to create a web server app that integrates both technologies
+"""
 
+from typing import Dict, Union
 from fastapi import FastAPI
-from pydantic import BaseModel
-import asyncio
+from pydantic import BaseModel  # pylint: disable=no-name-in-module
 from aredis import StrictRedis
 import uvicorn
 
 app = FastAPI()
-client = StrictRedis(host='127.0.0.1', port=6379, db=0)
+client = StrictRedis(host="127.0.0.1", port=6379, db=0)
+
 
 class CPE4(BaseModel):
+    """
+    Description of CPE4 elements
+    """
     ipv4_address: str
     port_end: int
     port_start: int
     speedboost_trace_id: str
     action: str
 
+
 class CPE6(BaseModel):
+    """
+    Description of CPE6 elements
+    arg BaseModel:
+    """
     ipv6_address: str
     port_end: int
     port_start: int
     speedboost_trace_id: str
     action: str
 
+
 class Item(BaseModel):
-    #cm_mac: str
+    """
+    Description of Item elements
+    """
+    # cm_mac: str
     qos_type: str
     router_ipv6_pd: str
     cpe_ipv4: CPE4
     cpe_ipv6: CPE6
 
-class application():
+
+class Application:
+    """
+        This Application will allow users to edit data into a redis hash table
+    """
+    @staticmethod
     @app.get("/")
     async def read_root():
-        return {"keys": await client.keys('*')}
+        """
+            This method is the default apps web page and displays all keys currently in redis
+        """
+        return {"keys": await client.keys("*")}
 
+    @staticmethod
     @app.get("/items/{item_id}")
     async def read_item(cm_mac: str):
+        """
+            This method will get the data a current mac address is
+            pointing to in the redis hash table
+                cm_mac: a str that represents a specific mac address
+        """
         hold = await client.get(cm_mac)
         item = Item.parse_raw(hold)
-
         return {
             "cm_mac": cm_mac,
             "item": item.dict(),
         }
 
+    @staticmethod
     @app.put("/items/{item_id}")
-    async def update_item(cm_mac: str, item: Item):
-        await client.getset(f'{cm_mac}', item.json())
+    async def update_item(cm_mac: str, item: Item) -> Dict[str, Union[str, CPE4, CPE6]]:
+        """
+            This method will replace the data a current mac address is pointing
+             to in the redis hash table
+                cm_mac: a str that represents a specific mac address
+                item: an Item object containing information regarding the mac address
+        """
+        await client.getset(f"{cm_mac}", item.json())
         return {
             "cm_mac": cm_mac,
             "qos_type": item.qos_type,
             "router_ipv6_pd": item.router_ipv6_pd,
             "cpe_ipv4": item.cpe_ipv4,
-            "cpe_ipv6": item.cpe_ipv6
+            "cpe_ipv6": item.cpe_ipv6,
         }
 
+    @staticmethod
     @app.post("/items/{item_id}")
-    async def post_item(cm_mac: str, item: Item):
-        await client.set(f'{cm_mac}', item.json())
-        assert await client.exists(f'{cm_mac}') is True
+    async def post_item(cm_mac: str, item: Item) -> Dict[str, Union[str, CPE4, CPE6]]:
+        """
+            This method will add a new mac address to the redis hash table
+                cm_mac: a str that represents a specific mac address
+                item: an Item object containing information regarding the mac address
+        """
+        await client.set(f"{cm_mac}", item.json())
+        assert await client.exists(f"{cm_mac}") is True
         return {
             "cm_mac": cm_mac,
             "qos_type": item.qos_type,
             "router_ipv6_pd": item.router_ipv6_pd,
             "cpe_ipv4": item.cpe_ipv4,
-            "cpe_ipv6": item.cpe_ipv6
+            "cpe_ipv6": item.cpe_ipv6,
         }
 
+    @staticmethod
     @app.delete("/items/{item_id}")
-    async def delete_item(cm_mac: str):
+    async def delete_item(cm_mac: str) -> Dict[str, str]:
+        """
+            This method will delete a specific mac address from the redis hash table
+                cm_mac: a str that represents a specific mac address
+        """
         await client.delete(cm_mac)
-        assert not await client.exists(f'{cm_mac}') is True
-        return {"message": f"cm mac address: {cm_mac} has been removed from the database"}
+        assert not await client.exists(f"{cm_mac}") is True
+        return {
+            "message": f"cm mac address: {cm_mac} has been removed from the database"
+        }
 
+    @staticmethod
     @app.delete("/delete_all/")
-    async def delete_all():
+    async def delete_all() -> Dict[str, str]:
+        """
+            This method will call flushdb() and remove all entries from the redis database
+        """
         await client.flushdb()
         return {
-            "message": f"All entries removed from database!",
-            "keys": await client.keys('*')
+            "message": "All entries removed from database!",
+            "keys": await client.keys("*"),
         }
 
+
 if __name__ == "__main__":
-    app = application()
-    uvicorn.run("fast_redis_combo:app", host="127.0.0.1", port=8000, log_level="info", workers=5)
+    app = Application()
+    uvicorn.run("fast_redis_combo:app", host="127.0.0.1", port=8000, log_level="info")
